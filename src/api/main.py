a3fbc70b7c6c0df2bf477e90a78d1e23622e855c
Snive6 <mateusz.kos1@edu.uekat.pl>
2022-12-07 17:26:27 +0100

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from ml_modules.extractive_text_summarizing import summarize
 
 app: FastAPI = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+@app.get("/get_text")
+async def root(text: str, length_of_summarization: float):
+    summarized_text = summarize(text)
+    return summarized_text
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
