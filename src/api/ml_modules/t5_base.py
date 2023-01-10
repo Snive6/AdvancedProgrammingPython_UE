905d626f033516ebcf4974ebf77c9ba3da140bc4
Snive6 <mateusz.kos1@edu.uekat.pl>
2023-01-10 23:49:00 +0100

    Update t5_base.py

--- src/api/ml_modules/t5_base.py
+++ src/api/ml_modules/t5_base.py
@@ -2,14 +2,14 @@ from transformers import pipeline
 import os
 
 
-def t5_summarizer(text, length):
+def t5_summarizer(text, max_length):
     summarizer = pipeline("summarization", model="t5-base", tokenizer="t5-base", framework="tf")
     # summarizer = pipeline("summarization")
-    summary = summarizer(text_example, max_length=length)[0]['summary_text']
+    summary = summarizer(text, max_length=max_length)[0]['summary_text']
     return summary
 
 
 if __name__ == '__main__':
     text_example = open("src/api/example/text_example", "r").read()
-    summary_text = t5_summarizer(text_example, length=100)
+    summary_text = t5_summarizer(text_example, max_length=100)
     print(summary_text)
