0858f8e1167fdff51bd63dd2b1a08af292529ea6
Snive6 <mateusz.kos1@edu.uekat.pl>
2023-01-11 21:23:26 +0100

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,26 +1,64 @@
 from fastapi import FastAPI, HTTPException, Depends, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from ml_modules.extractive_text_summarizing import summarize as default_summarizer
 from ml_modules.pegasus import pegasus_summarizer
 from ml_modules.bart_summarizer import bart_summarizer
 from typing import Optional
 from enum import Enum
 import uvicorn
+from pymongo import MongoClient
+import jwt
+import time
+from passlib.context import CryptContext
 
-app: FastAPI = FastAPI()
-security = HTTPBasic()
+app = FastAPI()
 
-users = {"trudnY": "PaC13Nt"}
+# Create a password context object to handle password hashing and verification
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@app.post("/login/")
-async def login(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = users.get(credentials.username)
-    if correct_username is None:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    if correct_username != credentials.password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    return {"message": "Welcome"}
+# Dependency function to get database connection
+def get_db():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["ue_project_1"]
+    return db
+
+
+def create_test_user(db):
+    hashed_password = pwd_context.hash("test")
+    test_user = {"username": "test", "password": hashed_password}
+    user_collection = db["users"]
+    user_collection.insert_one(test_user)
+
+
+@app.on_event("startup")
+async def startup():
+    db = get_db()
+    create_test_user(db)
+
+
+# Function to authenticate a user by checking their username and password
+async def authenticate_user(username: str, password: str, db: MongoClient = Depends(get_db)):
+    user_collection = db["users"]
+    user = user_collection.find_one({"username": username})
+    if not user or not pwd_context.verify(password, user["password"]):
+        raise HTTPException(status_code=400, detail="Invalid username or password")
+    return user
+
+
+# Login endpoint
+@app.post("/login")
+async def login(username: str, password: str):
+    user = await authenticate_user(username, password)
+
+    # Create a JWT token that contains the user's username and expires in 15 minutes
+    access_token = jwt.encode({"sub": user["username"], "exp": int(time.time() + 900)}, "secret", algorithm="HS256")
+
+    return {"access_token": access_token.decode()}
+
+
+@app.post("/logout")
+async def logout():
+    return {"message": "User has been logged out"}
 
 
 class ModelName(str, Enum):
@@ -36,9 +74,16 @@ responses = {
 }
 
 
-@app.get("/get_text", status_code=status.HTTP_200_OK,
+@app.get("/summarize", status_code=status.HTTP_200_OK,
          responses={**responses})
-async def root(text: str, model_name: ModelName, length_of_summarization: Optional[float] = 0.6):
+async def root(text: str, model_name: ModelName, access_token, db: MongoClient = Depends(get_db),
+               length_of_summarization: Optional[float] = 0.6):
+    try:
+        payload = jwt.decode(access_token, "secret", algorithms=["HS256"])
+    except (jwt.DecodeError, jwt.ExpiredSignatureError):
+        raise HTTPException(status_code=401, detail="Invalid token")
+    username = payload.get("sub")
+
     summarized_text = ''
     match model_name:
         case ModelName.extractive_summarizer:
@@ -47,8 +92,29 @@ async def root(text: str, model_name: ModelName, length_of_summarization: Option
             summarized_text = pegasus_summarizer(text, max_length=length_of_summarization*100)[0]
         case ModelName.bart:
             summarized_text = bart_summarizer(text, max_length=length_of_summarization*100)
-    return summarized_text
 
+    # Save the summary and original text to the database
+    collection = db["summaries"]
+    collection.insert_one({"summary": summarized_text, "original_text": text, "username": username})
+
+    try:
+        collection.insert_one({"summary": summarized_text, "original_text": text, "username": username})
+    except:
+        raise HTTPException(status_code=500, detail="An error occurred while saving the results to the database")
+
+    return {"summary": summarized_text}
+
+@app.get("/summaries")
+async def get_summaries(access_token: str, db: MongoClient = Depends(get_db)):
+    try:
+        payload = jwt.decode(access_token, "secret", algorithms=["HS256"])
+    except (jwt.DecodeError, jwt.ExpiredSignatureError):
+        raise HTTPException(status_code=401, detail="Invalid token")
+    username = payload.get("sub")
+    # Find all summaries for the current user in the database
+    collection = db["summaries"]
+    summaries = collection.find({"username": username})
+    return list(summaries)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', reload=True)
