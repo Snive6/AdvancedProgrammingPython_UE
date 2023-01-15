5026518a2753f2e864ae571871c4310babaa420d
Snive6 <mateusz.kos1@edu.uekat.pl>
2023-01-16 00:00:43 +0100

    Create bart.py

---
+++ src/api/ml_modules/bart.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+import os
+
+
+def bart_summarizer(text, max_length):
+    summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+    summary = summarizer(text, max_length=max_length)[0]['summary_text']
+    return summary
+
+
+if __name__ == '__main__':
+    text_example = open("example/text_example", "r").read()
+    summary_text = bart_summarizer(text_example, max_length=100)
+    print(summary_text)
