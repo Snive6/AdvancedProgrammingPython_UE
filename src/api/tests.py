e16bd5b25c91d472d35227e0111e06deadcc11c2
Snive6 <mateusz.kos1@edu.uekat.pl>
2023-01-16 22:15:56 +0100

    Update tests.py

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,5 +1,8 @@
+import pymongo
 import pytest
+from fastapi import HTTPException
 from fastapi.testclient import TestClient
+from main import authenticate_user, create_test_user, pwd_context
 from jwt.exceptions import ExpiredSignatureError, DecodeError
 from jwt import decode
 from pymongo import MongoClient
@@ -8,6 +11,8 @@ import jwt
 from ml_modules.pegasus import pegasus_summarizer
 from ml_modules.extractive_text_summarizing import extractive_summarizer
 from ml_modules.bart import bart_summarizer
+from main import get_db
+import uvicorn
 
 
 def test_bart_summarizer():
@@ -49,30 +54,69 @@ def test_pegasus_summarizer():
     assert summary == [expected_summary]
 
 
-@pytest.fixture()
-def client():
-    return TestClient(app)
+client = TestClient(app)
 
 
-# TODO: complete it. Returns status 422
-def test_get_summaries_valid_token(client):
-    # Create a valid token
-    access_token = jwt.encode({"sub": "test"}, "secret", algorithm="HS256")
-    # Insert a test summary into the database
-    db = MongoClient("mongodb+srv://admin:admin@cluster0.n8heu6j.mongodb.net/?retryWrites=true&w=majority").testdb
-    db.summaries.insert_one({"username": "test", "summary": "test summary", "original_text": "test text"})
-
-    response = client.get("/summaries", headers={"Authorization": f"Bearer {access_token}"})
+def test_login():
+    response = client.post("/login?username=test&password=test")
     assert response.status_code == 200
-    assert response.json() == [{"_id": "test_id", "username": "test", "summary": "test summary",
-                                "original_text": "test text"}]
+    json_response = response.json()
+    assert "access_token" in json_response
+    access_token = json_response["access_token"]
+    payload = jwt.decode(access_token, "secret", algorithms=["HS256"])
+    assert payload["sub"] == "test"
+    assert "exp" in payload
 
 
-# TODO: complete it. Returns status 422
-def test_get_summaries_invalid_token(client):
-    # Create an invalid token
-    access_token = "invalid_token"
+def test_get_summaries():
+    # First, login to get an access token
+    login_response = client.post("/login?username=test&password=test")
+    json_response = login_response.json()
+    access_token = json_response["access_token"]
 
-    response = client.get("/summaries", headers={"Authorization": f"Bearer {access_token}"})
-    assert response.status_code == 401
-    assert response.json() == {"detail": "Invalid token"}
+    # Now, call the /summaries endpoint with the access token
+    response = client.get(f"summaries?access_token={access_token}")
+
+    # Assert that the response has a status code of 200 OK
+    assert response.status_code == 200
+
+    # Assert that the response body contains the expected data
+    json_response = response.json()
+    assert isinstance(json_response, list)
+    assert all(isinstance(summary, dict) for summary in json_response)
+    assert all("_id" in summary for summary in json_response)
+    assert all("username" in summary for summary in json_response)
+    assert all("summary" in summary for summary in json_response)
+    assert all("original_text" in summary for summary in json_response)
+
+
+def test_summarize():
+    # First, create a test user and log in to get an access token
+    test_user = {"username": "test", "password": pwd_context.hash("test")}
+    db = get_db()
+    user_collection = db["users"]
+    user_collection.insert_one(test_user)
+    login_response = client.post("/login?username=test&password=test")
+    access_token = login_response.json()["access_token"]
+
+    # Now, call the /summarize endpoint with a test text and model name
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, " \
+           "dignissim sit amet, adipiscing nec, ultricies sed, dolor."
+    response = client.get("/summarize", params={"text": text, "model_name": "extractive_summarizer",
+                                                "access_token": access_token})
+    assert response.status_code == 200
+    json_response = response.json()
+    assert "summary" in json_response
+
+    # Check if the summary was saved in the database
+    collection = db["summaries"]
+    summary = collection.find({'username': 'test'}).sort([("_id", pymongo.DESCENDING)]).limit(1)[0]
+    assert summary["summary"] == json_response["summary"]
+    assert summary["original_text"] == text
+
+
+if __name__ == '__main__':
+    db = get_db()
+    create_test_user(db)
+    uvicorn.run('main:app', reload=True)
+
