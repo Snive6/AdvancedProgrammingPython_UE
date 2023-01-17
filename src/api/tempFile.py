139339365df3ff13434da9a6e84073f4c5ed8cf3
Snive6 <mateusz.kos1@edu.uekat.pl>
2023-01-17 20:49:34 +0100

    Create tempFile.py

---
+++ src/api/tempFile.py
@@ -0,0 +1,6 @@
+from ml_modules.pegasus import PegasusSummarizer
+
+ps = PegasusSummarizer()
+text_example = open("ml_modules/example/text_example", "r").read()
+summary = ps.summarize(text_example, max_length=100)
+print(summary)
