cfd6e9ca9e313ed0ed04f263aacf20433d3e1688
Snive6 <mateusz.kos1@edu.uekat.pl>
2023-01-17 20:49:27 +0100

    Update bart.py

--- src/api/ml_modules/bart.py
+++ src/api/ml_modules/bart.py
@@ -1,13 +1,25 @@
 from transformers import pipeline
 
 
-def bart_summarizer(text, max_length):
-    bart_summarizer_model = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-    summary = bart_summarizer_model(text, max_length=max_length)[0]['summary_text']
-    return summary
+class BartSummarizer:
+    def __init__(self):
+        self.model = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+        self.model.model.eval()
+
+    def summarize(self, text, max_length):
+        summary = self.model(text, max_length=max_length)[0]['summary_text']
+        return summary
+
+
+# def bart_summarizer(text, max_length):
+#     bart_summarizer_model = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+#     summary = bart_summarizer_model(text, max_length=max_length)[0]['summary_text']
+#     return summary
 
 
 if __name__ == '__main__':
     text_example = open("example/text_example", "r").read()
-    summary_text = bart_summarizer(text_example, max_length=100)
+    bs = BartSummarizer()
+    summary_text = bs.summarize(text_example, max_length=100)
+    # summary_text = bart_summarizer(text_example, max_length=100)
     print(summary_text)
