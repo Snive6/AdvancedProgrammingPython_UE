b463a597f6149b08832ef56e36c6a20eac021134
Snive6 <mateusz.kos1@edu.uekat.pl>
2023-01-24 09:51:29 +0100

    Update modules

--- src/api/ml_modules/bart.py
+++ src/api/ml_modules/bart.py
@@ -11,12 +11,6 @@ class BartSummarizer:
         return summary
 
 
-# def bart_summarizer(text, max_length):
-#     bart_summarizer_model = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-#     summary = bart_summarizer_model(text, max_length=max_length)[0]['summary_text']
-#     return summary
-
-
 if __name__ == '__main__':
     text_example = open("example/text_example.txt", "r").read()
     bs = BartSummarizer()
