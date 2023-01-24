c4f6066148d088d8f1aa0bb321564e052416d240
Mateusz K <68960340+Snive6@users.noreply.github.com>
2023-01-24 09:53:40 +0100

    Update modules (#11)

--- src/api/ml_modules/pegasus.py
+++ src/api/ml_modules/pegasus.py
@@ -2,17 +2,6 @@ from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import torch
 
 
-# def pegasus_summarizer(text, max_length):
-#     model_name = 'google/pegasus-xsum'
-#     torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#     tokenizer = PegasusTokenizer.from_pretrained(model_name)
-#     model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
-#     batch = tokenizer.prepare_seq2seq_batch(text, truncation=True, padding='longest', return_tensors='pt',
-#                                             max_length=max_length)
-#     translated = model.generate(**batch)
-#     tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-#     return tgt_text
-
 class PegasusSummarizer:
     def __init__(self):
         self.model_name = 'google/pegasus-xsum'
